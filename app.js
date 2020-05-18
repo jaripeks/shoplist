aa71e32b10cc09948d805ee5fbf4d140933fff75
jaripeks <jari.p.salo@gmail.com>
2020-05-18 20:55:15 +0300

    added config and linting for tests

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const logger = require('./utils/logger')
+//const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
 //mongoose stuff here
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(middleware.requestLogger)
 
 //routes here
 
 app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,17 +1,28 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
-    logger.info(`method: ${request.method} | path: ${request.path}`)
-    logger.info(`body: ${JSON.stringify(request.body)}`)
-    logger.info('------------')
-    next()
+	logger.info(`method: ${request.method} | path: ${request.path}`)
+	logger.info(`body: ${JSON.stringify(request.body)}`)
+	logger.info('------------')
+	next()
 }
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+	if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+	logger.error(error.message)
+
+	next(error)
 }
 
 module.exports = {
-    requestLogger,
-    unknownEndpoint
+	requestLogger,
+	unknownEndpoint,
+	errorHandler
 }
