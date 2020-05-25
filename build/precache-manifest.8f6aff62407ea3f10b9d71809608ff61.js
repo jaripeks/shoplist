7dd6a5da6553feb33ffb9afb9e3087a79dc49b38
jaripeks <jari.p.salo@gmail.com>
2020-05-25 18:12:52 +0300

    frontend built

---
+++ build/precache-manifest.8f6aff62407ea3f10b9d71809608ff61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be57b700f751a580d911c6444428ce46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44c8b9b5dce94675863a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1798e91708edece3b2e5",
+    "url": "/static/js/2.f8587352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8587352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c8b9b5dce94675863a",
+    "url": "/static/js/main.8ec1dd40.chunk.js"
+  },
+  {
+    "revision": "6fc75f8d54b9bf0260d1",
+    "url": "/static/js/runtime-main.a5f2036a.js"
+  }
+]);
